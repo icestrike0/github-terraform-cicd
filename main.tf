de7c7c46a8761cedf7f967457d1ee1e1fb843c71
Training <training@ip-172-31-20-238.us-east-2.compute.internal>
2025-12-09 11:44:17 +0000

    Trigger CI/CD pipeline

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "azurerm" {
+  resource_group_name  = "terraform-backend-rg"
+  storage_account_name = "tfstatebackend344"
+  container_name       = "tfstate"
+  key                  = "terraform.tfstate"
+}
+
 }
 
 provider "azurerm" {
